01ca6f612833ab720e473f0c4b12701821f5476e
jelou <icidicf@gmail.com>
2015-08-06 21:41:45 -0700

    cisco utility

---
+++ utility_lyp/cisco/cisco_edit_server/login_to_vnc2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/expect
+
+
+
+if { $argc == 1 } {
+    set ddts [lindex $argv 0]
+} else {
+    set ddts DDTS
+ }
+
+
+send_user "ddts is $ddts\n"
+
+spawn ssh 10.74.59.217
+
+expect "*password:"
+send "lab123\n"
+
+
+expect "*$*"
+send "pwd\r"
+
+expect "*$*"
+send "cd /tftpboot/jelou\r"
+
+interact
+
+
