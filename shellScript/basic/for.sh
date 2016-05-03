700da9036efb6d1bf9f85d09bb39d3fe769408d0
icidicf <icidicf@gmail.com>
2016-05-03 04:23:10 -0700

    add for

---
+++ shellScript/basic/for.sh
@@ -0,0 +1,7 @@
+#!/bin/bash                                                                      
+
+for i in {10..30}                                                             
+do                                                                                  
+    rm core.dnshealthcheck.$i*
+done
+
